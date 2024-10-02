7ccdd8675131f92ebfc71872129822f862d16d70
harsh <harshrajput5629@gmail.com>
2024-10-02 23:32:50 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" this is java  program ");
+        System.out.println(" that is use in the jenkins crash Crouse");
+        System.out.println("this is the nice and wram day ");
+
+        System.out.println(new Date());
+    }
+}
