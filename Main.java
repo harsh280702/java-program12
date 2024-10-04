829fe6579d269f4d6f5d630ceece3b9acca801db
harsh <harshrajput5629@gmail.com>
2024-10-04 17:10:19 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,5 +8,12 @@ public class Main {
 
         System.out.println(new Date()); 
         System.out.println(new Date()); 
+        System.out.println("this is second time that is use in this is project that ");
+         int a = 2990;
+        int b = 23233;
+        int c = a+b;
+         System.out.println(" this is the result that is c " +c);
+
+
     }
 }
