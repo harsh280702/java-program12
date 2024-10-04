433bab02f219a44848551f042937d61b6132b115
harsh <harshrajput5629@gmail.com>
2024-10-04 17:17:02 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -15,6 +15,76 @@ public class Main {
          System.out.println(" this is the result that is c " +c);
 
          System.out.println(" this is pipeline project");
+         
+        System.out.println(" this is java  program ");
+        System.out.println(" that is use in the jenkins crash Crouse");
+        System.out.println("this is the nice and wram day ");
+
+        System.out.println(new Date()); 
+        System.out.println(new Date()); 
+        System.out.println("this is second time that is use in this is project that ");
+         int a = 2990;
+        int b = 23233;
+        int c = a+b;
+         System.out.println(" this is the result that is c " +c);
+
+         System.out.println(" this is pipeline project");
+         
+        System.out.println(" this is java  program ");
+        System.out.println(" that is use in the jenkins crash Crouse");
+        System.out.println("this is the nice and wram day ");
+
+        System.out.println(new Date()); 
+        System.out.println(new Date()); 
+        System.out.println("this is second time that is use in this is project that ");
+         int a = 2990;
+        int b = 23233;
+        int c = a+b;
+         System.out.println(" this is the result that is c " +c);
+
+         System.out.println(" this is pipeline project");
+         
+        System.out.println(" this is java  program ");
+        System.out.println(" that is use in the jenkins crash Crouse");
+        System.out.println("this is the nice and wram day ");
+
+        System.out.println(new Date()); 
+        System.out.println(new Date()); 
+        System.out.println("this is second time that is use in this is project that ");
+         int a = 2990;
+        int b = 23233;
+        int c = a+b;
+         System.out.println(" this is the result that is c " +c);
+
+         System.out.println(" this is pipeline project");
+         
+        System.out.println(" this is java  program ");
+        System.out.println(" that is use in the jenkins crash Crouse");
+        System.out.println("this is the nice and wram day ");
+
+        System.out.println(new Date()); 
+        System.out.println(new Date()); 
+        System.out.println("this is second time that is use in this is project that ");
+         int a = 2990;
+        int b = 23233;
+        int c = a+b;
+         System.out.println(" this is the result that is c " +c);
+
+         System.out.println(" this is pipeline project");
+         
+        System.out.println(" this is java  program ");
+        System.out.println(" that is use in the jenkins crash Crouse");
+        System.out.println("this is the nice and wram day ");
+
+        System.out.println(new Date()); 
+        System.out.println(new Date()); 
+        System.out.println("this is second time that is use in this is project that ");
+         int a = 2990;
+        int b = 23233;
+        int c = a+b;
+         System.out.println(" this is the result that is c " +c);
+
+         System.out.println(" this is pipeline project");
 
 
     }
